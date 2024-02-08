d09b3a16d5838618f95883acc52867c5f219b42f
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2024-02-08 09:38:17 +0530

    import java.util.*;
    public class Tickets {
        public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
            int a = sc.nextInt();
            
            if(a * 4 > 1000) System.out.println("NO");
            else System.out.println("YES");
        }
    }

---
+++ Tickets.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Tickets {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        
+        if(a * 4 > 1000) System.out.println("NO");
+        else System.out.println("YES");
+    }
+}
